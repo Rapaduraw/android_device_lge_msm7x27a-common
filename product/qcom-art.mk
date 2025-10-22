2c3b4eb72a9a44c68c13c7531e3aa4e5a296a19d
RodrigoAyuso <81887791+RodrigoAyuso@users.noreply.github.com>
2025-10-22 22:46:40 +0000

    msm7x27a-common: Enable for all

--- product/qcom-art.mk
+++ product/qcom-art.mk
@@ -2,9 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap="false"
 
-# ART properties for first generation
-ifeq ($(LGE_MSM7X27A_GENERATION),1)
+# ART properties
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter="speed-profile" \
     dalvik.vm.image-dex2oat-filter="speed"
-endif
